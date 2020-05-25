6ead7020a254a921f6fe280a8321a262935b8379
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-25 20:53:45 +0300

    Imported the Oculus Integration asset and set up XR Plugin Management. Tried really hard to get Clayxels to build for the Quest, ended up wasting a lot of time (might try again later though). Recreated chair with normal cubes.

---
+++ Assets/Core/Scripts/ClayxelReloader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ClayxelReloader : MonoBehaviour
+{
+    public Clayxels.ClayContainer clayContainer;
+
+    void Start()
+    {
+        clayContainer.init();
+    }
+    void Update()
+    {
+        clayContainer.forceUpdateAllChunks();
+    }
+}
