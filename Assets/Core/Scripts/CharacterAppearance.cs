53c26ed3a61866e960444a29e2026c1ff1e9463f
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-27 06:56:30 +0300

    Imported VRCinemaforMobile by Fatty War. Placed chairs in cinema and set up screen. Imported CivilCharactersPack by ElRed. Imported FinalIK by RootMotion. Created character appearance and input behaviour.

---
+++ Assets/Core/Scripts/CharacterAppearance.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class CharacterAppearance : MonoBehaviour
+{
+    public enum ikId
+    {
+        leftHand = 0,
+        rightHand = 1,
+        leftFoot = 2,
+        leftThigh = 3,
+        rightFoot = 4,
+        rightThigh = 5,
+        body = 6,
+        head = 7
+    }
+    [System.Serializable]
+    public struct TransformIK
+    {
+        public ikId id;
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+
+    public CharacterCustomization maleCharacter, femaleCharacter;
+    public bool isFemale;
+    public bool hideHead;
+
+    public Transform[] ikTargets;
+    public List<TransformIK> transformIKs = new List<TransformIK>();
+
+    void Update()
+    {
+        UpdateAppearance();
+        UpdateIK();
+    }
+
+    private void UpdateAppearance()
+    {
+        maleCharacter.gameObject.SetActive(!isFemale);
+        femaleCharacter.gameObject.SetActive(isFemale);
+        if (hideHead)
+        {
+            maleCharacter.HideParts(new string[] { "Head", "Hair" });
+            femaleCharacter.HideParts(new string[] { "Head", "Hair" });
+        }
+        else
+        {
+            maleCharacter.UnHideParts(new string[] { "Head", "Hair" }, CharacterCustomization.ClothesPartType.Hat);
+            femaleCharacter.UnHideParts(new string[] { "Head", "Hair" }, CharacterCustomization.ClothesPartType.Hat);
+        }
+    }
+    private void UpdateIK()
+    {
+        foreach (var transformIK in transformIKs)
+        {
+            var currentTarget = ikTargets[(int)transformIK.id];
+            currentTarget.localPosition = transformIK.position;
+            currentTarget.localRotation = transformIK.rotation;
+        }
+    }
+
+    public void SetIKTargets(params TransformIK[] targets)
+    {
+        foreach (var target in targets)
+        {
+            var index = transformIKs.FindIndex((transformIK) => { return transformIK.id == target.id; });
+            if (index >= 0)
+                transformIKs[index] = target;
+            else
+                Debug.LogError("IK Error");
+        }
+    }
+}
---
+++ Assets/Core/Scripts/CharacterInput.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityHelpers;
+
+public class CharacterInput : MonoBehaviour
+{
+    private Quaternion handCorrection = Quaternion.Euler(90, 0, 0);
+    
+    private CharacterAppearance Appearance { get { if (_Appearance == null) _Appearance = GetComponent<CharacterAppearance>(); return _Appearance; } }
+    private CharacterAppearance _Appearance;
+
+    public bool isVREnabled;
+    public GameObject ovrRig;
+    public Transform rightController, leftController;
+    public Transform headTarget;
+    public Transform ikParent;
+
+    void Update()
+    {
+        Appearance.hideHead = isVREnabled;
+        ovrRig.SetActive(isVREnabled);
+        
+        Appearance.SetIKTargets(
+            new CharacterAppearance.TransformIK()
+            {
+                id = CharacterAppearance.ikId.leftHand,
+                position = ikParent.InverseTransformPoint(leftController.position),
+                rotation = ikParent.InverseTransformRotation(leftController.rotation) * handCorrection
+            },
+            new CharacterAppearance.TransformIK()
+            {
+                id = CharacterAppearance.ikId.rightHand,
+                position = ikParent.InverseTransformPoint(rightController.position),
+                rotation = ikParent.InverseTransformRotation(rightController.rotation) * handCorrection
+            },
+            new CharacterAppearance.TransformIK()
+            {
+                id = CharacterAppearance.ikId.head,
+                position = ikParent.InverseTransformPoint(headTarget.position)
+            }
+        );
+    }
+}
